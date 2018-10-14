03e00976e765d79ff903f18bd126080d8dfbaf25
Andrew Mok <andrew.mok@lalamove.com>
2018-10-14 15:09:48 +0800

    ADD: Add unit test cases

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import fetchMock from 'fetch-mock';
-import Logger from '../src/index';
+import Logger, { traceError } from '../src/index';
 
 const config = {
   url: 'https://log.dev.lalamove.com/',
@@ -14,76 +14,165 @@ const config = {
 describe('lalamove-web-logger tests', () => {
   let log;
 
-  beforeAll(() => {
-    fetchMock
-      .mock((
-        url,
-        { headers }) => url === config.url && headers.Authorization === `Basic ${config.key}`, 200);
+  describe('initialized Logger with config', () => {
+    beforeAll(() => {
+      fetchMock
+        .mock(
+          (
+            url,
+            { headers }) => url === config.url && headers.Authorization === `Basic ${config.key}`,
+          200);
+    });
+
+    afterAll(() => {
+      // runs after all tests in this block
+    });
+
+    beforeEach(() => {
+      log = new Logger(config);
+    });
+
+    afterEach(() => {
+      // runs after each test in this block
+    });
+
+    test('should init default config', () => {
+      expect(log._config).toBe(config);
+    });
+
+    test('should change the location to TW_TPE', () => {
+      log.changeLocation('TW_TPE');
+      expect(log._config.location).toBe('TW_TPE');
+    });
+
+    test('should change the locale to zh_TW', () => {
+      log.changeLocale('zh_TW');
+      expect(log._config.locale).toBe('zh_TW');
+    });
+
+    test('should log message as info level', () => {
+      console.log = jest.fn();
+      const result = log.info('testing message');
+      expect(console.log).toHaveBeenCalledWith('[info] testing message');
+      expect(console.log).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.log.mockClear();
+    });
+
+    test('should log message as debug level', () => {
+      console.log = jest.fn();
+      const result = log.debug('debug message');
+      expect(console.log).toHaveBeenCalledWith('[debug] debug message');
+      expect(console.log).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.log.mockClear();
+    });
+
+    test('should log message as warning level', () => {
+      console.warn = jest.fn();
+      const result = log.warning('warning message');
+      expect(console.warn).toHaveBeenCalledWith('[warning] warning message');
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.warn.mockClear();
+    });
+
+    test('should log message as error level', () => {
+      console.error = jest.fn();
+      const result = log.error('error message');
+      expect(console.error).toHaveBeenCalledWith('[error] error message');
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.error.mockClear();
+    });
+
+    test('should log message as fatal level', () => {
+      console.error = jest.fn();
+      const result = log.fatal('fatal message');
+      expect(console.error).toHaveBeenCalledWith('[fatal] fatal message');
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.error.mockClear();
+    });
+
+    test('should call window.onerror', () => {
+      console.error = jest.fn();
+      const result = window.onerror('error message', 'index.js', 1, 1, { stack: 'track stack' });
+      expect(console.error).toHaveBeenCalledWith('[error] error message');
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(result).toBe(true);
+      console.error.mockClear();
+    });
+
+    test('should return false if no message defined', () => {
+      console.log = jest.fn();
+      const result = log.info();
+      expect(console.log).toHaveBeenCalledTimes(0);
+      expect(result).toBe(false);
+      console.log.mockClear();
+    });
   });
 
-  afterAll(() => {
-    // runs after all tests in this block
+  describe('initialized Logger with config and wrong key', () => {
+    beforeAll(() => {
+      fetchMock
+        .mock(
+          (
+            url,
+            { headers }) => url === config.url && headers.Authorization === `Basic ${config.key}`,
+          200)
+        .mock('*', 401);
+    });
+
+    test('should fail to post data request', async () => {
+      const { key: _, ...omitedConfig } = config;
+      omitedConfig.key = 'WRONG_KEY';
+      log = new Logger(omitedConfig);
+
+      try {
+        await log._post();
+      } catch (e) {
+        expect(e).toEqual('Unhandled error');
+      }
+    });
   });
 
-  beforeEach(() => {
-    log = new Logger(config);
+  describe('initialized Logger without config', () => {
+    test('expected error to be throw', () => {
+      expect(() => {
+        log = new Logger();
+      }).toThrow('[lalamove-web-logger] Missing configuration. Please check documentation for' +
+        ' the usage.');
+    });
   });
 
-  afterEach(() => {
-    // runs after each test in this block
-  });
+  describe('traceError tests', () => {
+    test('expected to return file, line, column and backtrace', () => {
+      const error = {
+        message: 'error message',
+        stack: 'Error: error message↵    at Home._this.render' +
+          ' (https://web.lalamove.com/static/js/bundle.js:1:1)'
+      };
+      const result = traceError(error);
+      expect(result).toEqual({
+        file: 'https://web.lalamove.com/static/js/bundle.js',
+        line: '1',
+        column: '1',
+        backtrace: error.stack,
+      });
+    });
 
-  test('should init default config', () => {
-    expect(log._config).toBe(config);
-  });
-
-  test('should change the location to TW_TPE', () => {
-    log.changeLocation('TW_TPE');
-    expect(log._config.location).toBe('TW_TPE');
-  });
-
-  test('should change the locale to zh_TW', () => {
-    log.changeLocale('zh_TW');
-    expect(log._config.locale).toBe('zh_TW');
-  });
-
-  test('should log message as info level', () => {
-    console.log = jest.fn();
-    const result = log.info('testing message');
-    expect(console.log).toHaveBeenCalledWith('[info] testing message');
-    expect(result).toBe(true);
-    console.log.mockClear();
-  });
-
-  test('should log message as debug level', ()=> {
-    console.log = jest.fn();
-    const result = log.debug('debug message');
-    expect(console.log).toHaveBeenCalledWith('[debug] debug message');
-    expect(result).toBe(true);
-    console.log.mockClear();
-  });
-
-  test('should log message as warning level', () => {
-    console.warn = jest.fn();
-    const result = log.warning('warning message');
-    expect(global.console.warn).toHaveBeenCalledWith('[warning] warning message');
-    expect(result).toBe(true);
-    console.warn.mockClear();
-  });
-
-  test('should log message as error level', () => {
-    console.error = jest.fn();
-    const result = log.error('error message');
-    expect(console.error).toHaveBeenCalledWith('[error] error message');
-    expect(result).toBe(true);
-    console.error.mockClear();
-  });
-
-  test('should log message as fatal level', () => {
-    console.error = jest.fn();
-    const result = log.fatal('fatal message');
-    expect(console.error).toHaveBeenCalledWith('[fatal] fatal message');
-    expect(result).toBe(true);
-    console.error.mockClear();
+    test('expected to return undefined file, line, column and backtrace if IE < 10', () => {
+      const error = {
+        message: 'error message',
+      };
+      const result = traceError(error);
+      expect(result).toEqual({
+        file: undefined,
+        line: undefined,
+        column: undefined,
+        backtrace: undefined,
+      });
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -14,24 +14,39 @@ const defaultConfig = {
   platform: null,
 };
 
-function traceError() {
-  const e = new Error();
+function traceError(e = new Error()) {
   if (!e.stack) try {
     // IE requires the Error to actually be throw or else the Error's 'stack' property is undefined.
     throw e;
   } catch (e) {
     if (!e.stack) {
       // IE < 10
-      return { filename: undefined, line: undefined, column: undefined, backtrace: undefined };
+      return {
+        filename: undefined,
+        line: undefined,
+        column: undefined,
+        backtrace: undefined,
+      };
     }
   }
-  const stack = e.stack.toString().split(/\r\n|\n/);
-  const backtrace = stack.map(msg => msg.trim()).join(' ');
+
+  const backtrace = e.stack.toString();
+  const stack = backtrace.split(/\r\n|\n/);
   const regex = /(http.*):(\d+):(\d+)/;
   const frame = stack.find(msg => regex.test(msg));
-  if (!frame) return { filename: undefined, line: undefined, column: undefined, backtrace: undefined };
+  if (!frame) return {
+    filename: undefined,
+    line: undefined,
+    column: undefined,
+    backtrace: undefined,
+  };
   const [_, file, line, column] = regex.exec(frame);
-  return { file, line, column, backtrace };
+  return {
+    file,
+    line,
+    column,
+    backtrace,
+  };
 }
 
 class Logger {
@@ -42,7 +57,9 @@ class Logger {
         ' for the usage.');
     }
     this._config = config;
-    if (window) window.onerror = this._handleOnError;
+    if (window) {
+      window.onerror = this._handleOnError;
+    }
   }
 
   async _post(data) {
@@ -55,10 +72,12 @@ class Logger {
         },
         body: JSON.stringify(data),
       });
-      console.log(this._config.url);
-      if (!response.ok) throw new Error('Unhandled error');
-      if (response.contentType === 'application/json') return response.json();
-      return response.text();
+
+      if (!response.ok) {
+        throw new Error('Unhandled error');
+      }
+
+      return response;
     } catch (e) {
       console.error(e);
     }
@@ -67,13 +86,19 @@ class Logger {
   _logger = (level) => (message, file, line, col, err, context) => {
     let [sourceFile, sourceLine] = [file, line];
     let backtrace = err && err.stack;
+
     if (!sourceFile && !sourceLine) {
       const stack = traceError();
       sourceFile = stack.file || 'unknown';
       sourceLine = stack.line || 0;
-      if (!backtrace) backtrace = stack.backtrace || 'unknown';
+      if (!backtrace) {
+        backtrace = stack.backtrace || 'unknown';
+      }
     }
-    if (!message) message = 'unknown';
+
+    if (!message) {
+      return false;
+    }
 
     switch (level) {
       case LEVEL_WARNING:
@@ -100,7 +125,7 @@ class Logger {
         location: this._config.location,
         environment: this._config.environment,
         platform: this._config.platform,
-        agent: (navigator && navigator.userAgent) ? navigator.userAgent : 'unknown',
+        agent: navigator && navigator.userAgent,
       },
       backtrace,
     };
@@ -112,24 +137,28 @@ class Logger {
   _handleOnError = (message, file, line, col, err) =>
     this._logger(LEVEL_ERROR)(message, file, line, col, err);
 
-  changeLocation(location = defaultConfig.location) {
+  changeLocation(location) {
     this._config.location = location;
   }
 
-  changeLocale(locale = defaultConfig.locale) {
+  changeLocale(locale) {
     this._config.locale = locale;
   }
 
   info = (message, context) =>
     this._logger(LEVEL_INFO)(message, null, null, null, null, context);
+
   debug = (message, context) =>
     this._logger(LEVEL_DEBUG)(message, null, null, null, null, context);
+
   warning = (message, context) =>
     this._logger(LEVEL_WARNING)(message, null, null, null, null, context);
+
   error = (message, context, errStack) =>
     this._logger(LEVEL_ERROR)(message, null, null, null, { stack: errStack }, context);
+
   fatal = (message, context, errStack) =>
     this._logger(LEVEL_FATAL)(message, null, null, null, { stack: errStack }, context);
 }
 
-export default Logger;
+export { Logger as default, traceError };
